07613d676c6d60de9b575ee248bd5f088211c3a9
yassunsun <ww16471z@icloud.com>
2021-01-05 12:49:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
+  root to: "rooms#index"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
